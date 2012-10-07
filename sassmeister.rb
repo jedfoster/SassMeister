6256be9381bc0bc3cac55e3986b57c3f0eca8f5e
Jed Foster <jed@jedfoster.com>
2012-10-06 22:08:14 -0700

    Clean up.

--- sassmeister.rb
+++ sassmeister.rb
@@ -3,9 +3,7 @@
 require 'rubygems'
 require 'compass'
 require 'sass'
-require 'tempfile'
-
-require 'stipe'               # installed as a gem
+require 'stipe'
 
 # If you're using bundler, you will need to add this
 require 'bundler/setup'
@@ -15,8 +13,6 @@ require 'sinatra/partial'
 
 set :partial_template_engine, :erb
 
-# Helpers to add a new horn section to the band
-# -----------------------------------------------------------------------
 helpers do
   include ERB::Util
   alias_method :code, :html_escape
@@ -25,10 +21,6 @@ end
 
 configure do
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
-
-
-  # disable :raise_errors
-  # disable :show_exceptions
 end
 
 # at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
@@ -42,15 +34,12 @@ get '/' do
   erb :index
 end
 
+
 post '/compile' do
   begin
-    send("#{params[:syntax]}".to_sym, params[:sass], {:style => :"#{params[:output]}"})
+    send("#{params[:syntax]}".to_sym, params[:sass].chomp, {:style => :"#{params[:output]}"})
   rescue Sass::SyntaxError => e
     status 200
     e.to_s
   end
-end
-
-error do
-  'Sorry there was a nasty error - ' + env['sinatra.error'].name
 end
