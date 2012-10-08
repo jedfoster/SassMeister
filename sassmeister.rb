f9199e5f2199f59c7ab85b45dbff9b321915883c
Jed Foster <jed@jedfoster.com>
2012-10-08 11:59:58 -0700

    Temporary fix. Turn off runtime compilation of app's Sass.

--- sassmeister.rb
+++ sassmeister.rb
@@ -29,15 +29,15 @@ helpers do
 end
 
 
-configure do
-  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
-end
+# configure do
+#   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
+# end
 
-# at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
-get '/stylesheets/:name.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  scss(:"../sass/#{params[:name]}", Compass.sass_engine_options )
-end
+# # at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
+# get '/stylesheets/:name.css' do
+#   content_type 'text/css', :charset => 'utf-8'
+#   scss(:"../sass/#{params[:name]}", Compass.sass_engine_options )
+# end
 
 
 get '/' do
