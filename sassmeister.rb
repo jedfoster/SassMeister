75d5b78caa77566c90f84ef2105a6fa847ca35a4
Jed Foster <jed@jedfoster.com>
2012-10-21 19:26:40 -0700

    Completed GitHub OAuth integration.

--- sassmeister.rb
+++ sassmeister.rb
@@ -9,6 +9,7 @@ require 'sinatra'
 require 'sinatra/partial'
 require 'json'
 require 'github_api'
+require 'yaml'
 
 require 'sass'
 require 'compass'
@@ -23,6 +24,7 @@ require 'susy'
 
 set :partial_template_engine, :erb
 
+enable :sessions
 
 helpers do
   include ERB::Util
@@ -68,17 +70,20 @@ helpers do
       e.to_s
     end
   end
+
+  def github(auth_token = '')
+    gh_config = YAML.load_file("github.yml")
+
+    github = Github.new do |config|
+      config.client_id = gh_config['client_id']
+      config.client_secret = gh_config['client_secret']
+      config.oauth_token = auth_token
+    end
+  end
 end
 
-
-enable :sessions
-
-get '/foo' do
-  "value = " << session[:value].inspect
-end
-
-get '/foo/:value' do
-  session[:value] = params[:value]
+before '/*' do
+  @github = github(session[:github])
 end
 
 
@@ -101,29 +106,33 @@ get '/thankyou' do
 end
 
 
+get '/authorize' do
+  redirect to github.authorize_url :scope => 'gist'
+end
+
+
+get '/authorize/return' do
+  token = github.get_token(params[:code])
+
+  session[:github] = token.token
+
+  redirect to('/')
+end
+
+
 post '/gist/?:edit?' do
   sass = import_plugin(params)
   css = compile_sass(params, sass)
 
-  # Downloaded from SassMeister.com
+  github = github(session[:github])
 
+  # Downloaded from SassMeister.com
 
   sass_file = "SassMeister.#{params[:syntax]}"
   css_file = "SassMeister.css"
 
-  github = Github::Gists.new
-
-  # if params[:edit]
-  #   data = github.fork(session[:gist], files: {
-  #     css_file => {
-  #       content: "#{css}"
-  #     },
-  #     sass_file => {
-  #       content: "#{sass}"
-  #     }
-  #   })
-  # else
-    data = github.create(description: "Gist off", public: true, files: {
+  if params[:edit]
+    data = github.gists.edit(session[:gist], files: {
       css_file => {
         content: "#{css}"
       },
@@ -131,9 +140,18 @@ post '/gist/?:edit?' do
         content: "#{sass}"
       }
     })
-  # end
+  else
+    data = github.gists.create(description: "Gist off", public: true, files: {
+      css_file => {
+        content: "#{css}"
+      },
+      sass_file => {
+        content: "#{sass}"
+      }
+    })
+  end
 
-  session[:gist] = data.id
+  session[:gist] = data.id.to_s
 
   "https://gist.github.com/#{data.id}"
 end
