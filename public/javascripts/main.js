7a4e5ebd8efeb23ce7df25f5e5b5fea44d2a43f1
Dale Sande <dale.sande@gmail.com>
2012-11-20 16:05:30 -0800

    new header and footer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -114,7 +114,7 @@ http://github.com/bgrins/bindWithDelay
     // console.log( $("html").width());
     
     if ($("html").width() > 50 * 18) {
-      var html = $("html").height(), header = $(".site_header").height(), footer = $(".site_footer").height() * 0, controls = $('.sass_input .controls').height() * -1;
+      var html = $("html").height(), header = $(".site_header").height(), footer = $(".site_footer").height() * 2, controls = $('.sass_input .controls').height() * -1;
   
       $('.pre_container, .ace_scroller').css('height', html - header - footer - controls);
     }
