1f162c9f77c70fce8d073b698df58e45552f1257
Jed Foster <jed@jedfoster.com>
2013-04-07 06:43:23 -0800

    Remove commented code

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,9 +9,6 @@ before_fork do |server, worker|
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
-
-  # defined?(ActiveRecord::Base) and
-    # ActiveRecord::Base.connection.disconnect!
 end  
 
 after_fork do |server, worker|
@@ -19,7 +16,4 @@ after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-
-  # defined?(ActiveRecord::Base) and
-    # ActiveRecord::Base.establish_connection
 end
