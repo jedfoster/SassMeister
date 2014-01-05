015269ae6de8df53ae9ccdeaa92f9ae7b3cac531
Jed Foster <jed@jedfoster.com>
2014-01-05 14:12:31 -0800

    Significant refactor of messages.js

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -6,6 +6,13 @@ if (!Number.isInteger) {
   };
 }
 
+Object.size = function(obj) {
+  var size = 0, key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) size++;
+  }
+  return size;
+};
 
 (function($) {
   Messenger.options = {
@@ -16,37 +23,47 @@ if (!Number.isInteger) {
     }
   };
 
-  var randomMsg = function() {
-    msgs = [
-      'Show off your sassy side with a SassMeister sticker.',
-      'Get sweet, sassy stickers &amp; help support SassMeister.'
-    ];
-
-    return msgs[Math.floor(Math.random() * msgs.length)];
-  };
-  
- 
-  var stickerMsg = '<a href="http://devswag.com/products/sassmeister-stickers-4">\
-<h1>Swag message! </h1>\
-<p><img src="/images/sassmeister-detail-v02_medium.jpg"> ' + randomMsg() + '</p>\
-</a>';
-
 
   var messageCatalog = {
+    'libsass': {
+      visit_interval: 20,
+      time_interval: 86400 * 1,
+      impression_limit: -1,
+      content: '<h1>libSass is here!</h1><p>Click the <span class="control-icon" style="font-size: 0.66em;"><span class="alt">gear</span></span> icon above, and select "libSass" as the version.</p>',
+      messengerOptions: {
+        extraClasses: 'messenger-on-top',
+        postOptions: {hideAfter: 10}
+      },
+      conditions: function(stats) {
+        // If this message has been seen 5 times already, r
+        if(stats.messages[name] && stats.messages[name].impression_count == 5) {
+          return false;
+        }
+
+        return (!window.gist || (window.gist && window.gist.can_update_gist == true));
+      }
+    },
     'stickers': {
       visit_interval: 30,
       time_interval: 86400 * 7,
-      content: stickerMsg,
-      extraClasses: 'messenger-on-bottom swag-promo'
+      impression_limit: -1,
+      content: $('#promo .swag-promo').html(),
+      messengerOptions: {
+        extraClasses: 'messenger-on-bottom',
+        postOptions: {  }
+      },
+      conditions: function(stats) {
+        return true;
+      }
     }
-  };  
-    
+  };
+
 
   if($('body.about, body.thankyou').length < 1 ) {
     var messageStats = $.extend(true, {
       last_visit: Date.now(),
       visit_number: 0,
-      messages: []
+      messages: {}
     }, JSON.parse(localStorage.getItem('messageStats')) );
 
 
@@ -54,13 +71,57 @@ if (!Number.isInteger) {
 
 
     var queueAddMessage = function(name, message) {
+      // For now, because Messenger is dumb, I need to ensure only one message is in the queue.
+      // This means that only the first qualifying message in the catalog will be displayed.
+      // So, the more important message needs to be first in the catalog. Blech.
+      if (Object.size(messageQueue) >= 1) {
+        return;
+      }
+
       messageQueue[name] = message;
+
+      if (messageStats.messages[name]) {
+        $.extend(true, messageStats.messages[name], {
+          last_seen: Date.now(),
+          visit_number: messageStats.visit_number
+        });
+
+        messageStats.messages[name].impression_count++;
+      }
+      else {
+        messageStats.messages[name] = {
+          last_seen: Date.now(),
+          visit_number: messageStats.visit_number,
+          impression_count: 1
+        }
+      }
     };
+
+
+    var checkTimeIntervals = function(last_visit, interval) {
+      return last_visit < (Date.now() - interval * 1000);
+    };
+
+    var checkVisitIntervals = function(visits, interval) {
+      return Number.isInteger(visits / interval);
+    };
+
+    var checkImpressionLimits = function(stats, name, limit) {
+      if (limit == -1) {
+        return true;
+      }
+
+      return ! (stats.messages[name] && stats.messages[name].impression_count >= limit);
+    }
 
 
     // loop through messageCatalog and check if it is contained in messageStats.messages
     $.each(messageCatalog, function(name, message) {
-      if((messageStats.last_visit < (Date.now() - message.time_interval * 1000) ) || (Number.isInteger(messageStats.visit_number / message.visit_interval))){
+      if( (checkTimeIntervals(messageStats.last_visit, message.time_interval) || checkVisitIntervals(messageStats.visit_number, message.visit_interval))
+
+      && checkImpressionLimits(messageStats, name, message.impression_limit)
+
+      && message.conditions(messageStats) ) {
         // display a message
         queueAddMessage(name, message);
       }
@@ -80,15 +141,12 @@ if (!Number.isInteger) {
     localStorage.setItem('messageStats', JSON.stringify( messageStats ));
 
 
-    // Add promo message to the control panel
-    // This is an odd duck. Might want to consider setting this server-side
-    $('#promo .swag-promo').html(stickerMsg);
-    
-    
     // Loop through queued messages and display them.
     // May need a refactor as I've only tested with a single message
-    $.each(messageQueue, function(i, message) {      
-      Messenger({ extraClasses: message.extraClasses }).post({ message: message.content });
+    $.each(messageQueue, function(i, message) {
+      var postHash = $.extend(true, {message: message.content}, message.messengerOptions.postOptions);
+
+      Messenger({ extraClasses: message.messengerOptions.extraClasses }).post(postHash);
     });
   }
 })(jQuery);
