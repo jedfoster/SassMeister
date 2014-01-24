1455c0330bb249914edb4bb5aa056ad1726fa313
Jed Foster <jed@jedfoster.com>
2014-01-23 16:45:02 -0800

    Fix the viewport.js Chrome dev fix in Firefox

--- public/javascripts/viewport.js
+++ public/javascripts/viewport.js
@@ -2,17 +2,17 @@
   window.setViewportSize = function() {
     var size;
     window.viewportSize = 'desktop';
-    if (window.getComputedStyle) {
-      if(size = window.getComputedStyle(document.body, ':after').content.trim()) {
-        return window.viewportSize = size;
+    if (window.getComputedStyle) { // IE 8 doesn't support this method. Because it sucks.
+      // Chrome 34 (dev) has a broken getComputedStyle, so size will actually be null
+      if (size = window.getComputedStyle(document.body, ':after').content.trim()) {
+        return window.viewportSize = size.match(/\w+/)[0]; // Firefox includes extra quotes
       }
     }
   };
 
+  // Fires on every window resize
   $(window).on('resize', window.setViewportSize);
 
+  // Fire now, to init window.viewportSize
   window.setViewportSize();
 })(jQuery);
-
-
-
