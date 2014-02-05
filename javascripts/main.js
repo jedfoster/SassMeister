e2eceada8d09f887da20494480711e1b198b2a3c
Jed Foster <jed@jedfoster.com>
2014-02-04 19:52:00 -0800

    Replace Spin.js with a CSS spinner

--- javascripts/main.js
+++ javascripts/main.js
@@ -260,28 +260,6 @@ if($('body.app, body.embedded').length > 0 ) {
   });
 
 
-  if($.fn.spin) {
-    $('.spinner').spin({
-      lines: 9, // The number of lines to draw
-      length: 4, // The length of each line
-      width: 3, // The line thickness
-      radius: 4, // The radius of the inner circle
-      corners: 1, // Corner roundness (0..1)
-      rotate: 0, // The rotation offset
-      direction: 1, // 1: clockwise, -1: counterclockwise
-      color: '#666', // #rgb or #rrggbb or array of colors
-      speed: 1, // Rounds per second
-      trail: 60, // Afterglow percentage
-      shadow: false, // Whether to render a shadow
-      hwaccel: true, // Whether to use hardware acceleration
-      className: 'spinner', // The CSS class to assign to the spinner
-      zIndex: 2e9, // The z-index (defaults to 2000000000)
-      top: '2', // Top position relative to parent in px
-      left: '-26' // Left position relative to parent in px
-    });
-  }
-
-
   window.onmessage = function (event) {
     if (SassMeister.layout.html == 'show') {
       if(SassMeister.outputs.html) {
