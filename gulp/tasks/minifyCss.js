99390b4296a0deaf2e88bb74612a06a62a98e227
Jed Foster <jed@jedfoster.com>
2015-06-15 15:43:20 -0700

    Tweak gulp production tasks

--- gulp/tasks/minifyCss.js
+++ gulp/tasks/minifyCss.js
@@ -2,10 +2,13 @@ var gulp      = require('gulp');
 var config    = require('../config').production;
 var minifyCSS = require('gulp-minify-css');
 var size      = require('gulp-filesize');
+var rename    = require('gulp-rename');
 
 gulp.task('minifyCss', ['sass'], function() {
   return gulp.src(config.cssSrc)
-    .pipe(minifyCSS({keepBreaks:true}))
-    .pipe(gulp.dest(config.dest))
+    .pipe(minifyCSS())
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest(config.dest + '/css/'))
     .pipe(size());
-})
+});
+
--- gulp/tasks/uglifyJs.js
+++ gulp/tasks/uglifyJs.js
@@ -1,11 +1,21 @@
 var gulp    = require('gulp');
 var config  = require('../config').production;
 var size    = require('gulp-filesize');
-var uglify = require('gulp-uglify');
+var uglify  = require('gulp-uglify');
+var del     = require('del');
+var rename    = require('gulp-rename');
 
-gulp.task('uglifyJs', ['browserify'], function() {
-  return gulp.src(config.jsSrc)
-    .pipe(uglify())
-    .pipe(gulp.dest(config.dest))
-    .pipe(size());
+gulp.task('clean', function (cb) {
+  del(config.jsSrc, cb);
 });
+
+gulp.task('uglifyJs', ['browserify', 'clean'], function() {
+  return setTimeout(function() {
+    return gulp.src(config.jsSrc)
+      .pipe(uglify())
+      .pipe(rename('app.min.js'))
+      .pipe(gulp.dest(config.dest + '/js/'))
+      .pipe(size());
+  }, 50);
+});
+
