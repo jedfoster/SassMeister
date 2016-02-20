102efda839ba86e571501ff9f7477d6e8d46e3f0
Jed Foster <jed@jedfoster.com>
2016-02-20 11:36:39 -0800

    Fix production task

--- gulp/tasks/uglifyJs.js
+++ gulp/tasks/uglifyJs.js
@@ -4,18 +4,21 @@ var size    = require('gulp-filesize');
 var uglify  = require('gulp-uglify');
 var del     = require('del');
 var rename    = require('gulp-rename');
+var runSequence = require('run-sequence');
 
-gulp.task('clean', function (cb) {
+gulp.task('_clean', function (cb) {
   del(config.jsSrc, cb);
 });
 
-gulp.task('uglifyJs', ['browserify', 'clean'], function() {
-  return setTimeout(function() {
-    return gulp.src(config.jsSrc)
-      .pipe(uglify())
-      .pipe(rename('app.min.js'))
-      .pipe(gulp.dest(config.dest + '/js/'))
-      .pipe(size());
-  }, 50);
+gulp.task('_uglify', function() {
+  return gulp.src(config.jsSrc)
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(config.dest + '/js/'))
+    .pipe(size());
+});
+
+gulp.task('uglifyJs', function() {
+  runSequence('_clean', 'webpack', '_uglify');
 });
 
