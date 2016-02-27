6d406a6295cf62363f92b988a113e42831ccea22
Jed Foster <jed@jedfoster.com>
2016-02-27 11:38:45 -0800

    Really, really fix Mustache in markup task

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -6,7 +6,13 @@ var yaml = require('js-yaml');
 var fs   = require('fs');
 var replace = require('gulp-batch-replace');
 
-gulp.task('markup', function() {
+gulp.task('jade', function() {
+  return gulp.src(config.src)
+    .pipe(jade())
+    .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('markup', ['jade'], function() {
   var env = process.env.NODE_ENV || 'development',
       data = yaml.safeLoad(fs.readFileSync(__dirname + '/../../config/config.yml', 'utf8'))[env],
       manifest = JSON.parse(fs.readFileSync(config.dest + '/rev-manifest.json', 'utf8')),
@@ -16,8 +22,8 @@ gulp.task('markup', function() {
     replacements = Object.keys(manifest).map(function (key) {return [key, manifest[key]]});
   }
 
-  return gulp.src(config.src)
-    .pipe(jade())
+  return gulp.src(config.dest + '/*.html')
+    .pipe(replace([['{%&amp;', '{%&']]))
     .pipe(mustache(data, {tags: ['{%', '%}']}))
     .pipe(replace(replacements))
     .pipe(gulp.dest(config.dest));
